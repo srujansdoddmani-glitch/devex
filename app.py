f0a9df02cf512e29cebfae21152ef009630ff3d2
srujansdoddmani-glitch <srujansdoddmani@gmail.com>
2026-02-06 10:48:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Devex AI",
+    layout="wide"
+)
+
+st.title("📊 Devex AI")
+st.subheader("Advanced TA + FA + Risk Engine")
+
+st.success("App loaded successfully 🚀")
+
+st.info("Step 1 completed. No APIs connected yet.")
